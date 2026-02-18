6ea227bcb3b12f0eaa108677e050a3622a7336b2
Nico <mainvalcom@gmail.com>
2026-02-18 09:39:33 +0800

    update api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Personality;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/personalities', function () {
+    return Personality::all();
+});
+
+require __DIR__.'/auth.php';
